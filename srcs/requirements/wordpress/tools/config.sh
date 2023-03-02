0e50a3aaf84ab298be21e733704a179abe1b7ca7
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-03-02 13:30:26 +0100

    config php

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -3,4 +3,4 @@
 
 
 # run php-fpm7.3 listening for CGI request/ as a FastCGI process
-exec /usr/sbin/php-fpm7.3 -F -R
+exec /usr/sbin/php-fpm7.3 -F -R #Les flags servent à run en foreground, nécessaire dans le container docker
