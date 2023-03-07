d2c3d94cd8e12de4c0db2d51031dd02b3d8517f8
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-03-07 13:10:41 +0100

    add security for db script

--- srcs/requirements/mariadb/tools/database.sh
+++ srcs/requirements/mariadb/tools/database.sh
@@ -6,42 +6,43 @@ set -x #Turns on debugging for the current shell session : interprétation dans 
 # mysql -u root -e "SHOW GRANTS;" #shows grants for current user
 # mysql -u root -e "SHOW GRANTS FOR '${DB_USER_USERNAME}'@'localhost';"
 
-
-cat /etc/hosts
+# cat /etc/hosts
 
 ##################### DEMARRAGE #####################
 # démarrage du service mariaDB
 # /etc/init.d/mysql start
 service mysql start
 
-##################### CREATION USER, DATABASE  #####################
-# -e : Execute statement and quit.
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+if [ ! -d "/var/lib/mysql/${DB_NAME}" ]
+then
+    echo "create database and users"
+    ##################### CREATION USER, DATABASE  #####################
+    # -e : Execute statement and quit.
+    mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 
-# MySQL's permissions are based on the host. When you CREATE USER (or GRANT) the user into existence, you provide a host name.
-# It can be '%' (wildcard) or 'localhost' or any other IP or hostname
-# '%' (wildcard) : autorise l'accès pour n'importe quel host.
-# En l'occurence le host du conteneur wordpress pourra s'y connecter aussi !
-mysql -u root -e "CREATE USER IF NOT EXISTS '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+    # MySQL's permissions are based on the host. When you CREATE USER (or GRANT) the user into existence, you provide a host name.
+    # It can be '%' (wildcard) or 'localhost' or any other IP or hostname
+    # '%' (wildcard) : autorise l'accès pour n'importe quel host.
+    # En l'occurence le host du conteneur wordpress pourra s'y connecter aussi !
+    mysql -e "CREATE USER IF NOT EXISTS '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
 
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+    mysql -e "FLUSH PRIVILEGES;"
 
-# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-# mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASSWORD');"
-	
-# mysql -u root -p${DB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;" #Needed after altering user ?
+    #mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+    mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$DB_ROOT_PASSWORD');"
 
-# mysql -u root -p${DB_ROOT_PASSWORD} -e "SELECT host, user, password FROM mysql.user;"
-mysql -u root -e "SELECT host, user, password FROM mysql.user;"
+    #mysql -u root -e "FLUSH PRIVILEGES;" #Needed after altering user ?
+    #mysql -u root -p${DB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;" #Needed after altering user ?
 
-##################### SHUTDOWN #####################
-# mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
-sleep 1
-mysqladmin -u root shutdown
-# mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
-# /etc/init.d/mysql stop
+    # mysql -u root -p${DB_ROOT_PASSWORD} -e "SELECT host, user, password FROM mysql.user;"
 
+    ##################### SHUTDOWN #####################
+    sleep 3
+    # mysqladmin -u root shutdown
+    mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
+    # /etc/init.d/mysql stop
+fi
 
 ##################### REDEMARRAGE POUR LAISSER TOURNER #####################
 exec mysqld_safe #recommended way to start a mysqld server on Unix
-
